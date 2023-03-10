105cf57cfd2c04559b0196b4d4ad48aae52d7e77
Joakim <55298128+lethal1147@users.noreply.github.com>
2023-03-10 19:49:19 +0700

    Add script.js for Part2

---
+++ script.js
@@ -0,0 +1,16 @@
+// Challenge 1: Create an Age Calculator
+
+const birthYear = 2000;
+const futureYear = 2028;
+let age = futureYear - birthYear;
+console.log(`If you were born in ${birthYear}, then in ${futureYear} you’ll be ${age - 1} or ${age} (depending on the month in ${futureYear}).`)
+
+// Challenge 2: Create an information card
+
+const person = {
+    name: 'Kim',
+    location: 'Bangkok',
+    hobby: 'Play games',
+}
+
+console.log(`Hi, my name is ${person.name}. I live in ${person.location} and enjoy ${person.hobby}.`)
